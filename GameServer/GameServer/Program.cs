674f68bebfd4252ca0411c01ef01f581a281968e
MtotMtot <louisgbaker@hotmail.com>
2024-08-30 04:53:55 +0100

    Repo Created aftrer proj start
    Initial commit, basic server + client made. Client only connects to local IP "127.0.0.1"

---
+++ GameServer/GameServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GameServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Game Server";
+
+            Server.Start(4, 26950);
+
+            Console.ReadKey();
+        }
+    }
+}
